025e509bfc0d8ea31aceadca0f2d4b2052f36828
Stefan Costin <stefanfcostin@gmail.com>
2023-03-13 22:26:07 +0200

    Add command controller

---
+++ PizzaStore.Api/Controllers/CommandController.cs
@@ -0,0 +1,25 @@
+﻿using PizzaStore.Domain;
+using PizzaStore.Domain.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Beersender.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandController : ControllerBase
+    {
+        private readonly CommandRouter _router;
+
+        public CommandController(CommandRouter router)
+        {
+            _router = router;
+        }
+
+        [HttpPost]
+        public IActionResult PostCommand([FromBody] Command command)
+        {
+            _router.HandleCommand(command);
+            return Ok();
+        }
+    }
+}
